faf5958626deb1fe26c13d9b8b6b195ce0b62bcd
Danupol <a0899160664z@gmail.com>
2025-08-25 18:18:53 +0700

    init

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/My-web/', // <= แก้ตรงนี้
+  base: '/My-web/', // ให้ Vite รู้ว่าเวลาสร้างไฟล์ต้องอิง path นี้
   plugins: [react()],
 })
